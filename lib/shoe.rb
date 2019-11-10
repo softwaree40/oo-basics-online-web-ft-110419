c3b4b7eebbc0432a6079c2ef71383f6c7a7ff100
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-10 22:51:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -18,4 +18,10 @@ class Shoe
      def size 
        @size 
      end 
+     def material=(material)
+        @material = material 
+      end 
+      def material 
+        @material 
+      end 
 end
