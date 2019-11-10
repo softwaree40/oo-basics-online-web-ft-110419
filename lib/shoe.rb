0590f23b88049921f1728873b832923150fde685
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-10 22:52:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -24,4 +24,11 @@ class Shoe
       def material 
         @material 
       end 
+      def condition=(condition)
+          @condition = condition
+        end
+        def condition 
+          @condition
+        end 
+        
 end
