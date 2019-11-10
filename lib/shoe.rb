8295f971d418a5e59cbfa15027a4034da5f5361b
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-10 23:27:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -33,7 +33,7 @@ class Shoe
           
         end 
        def cobble
-         self.condition = "new"
+          self.condition = "new"
            puts "Your shoe is as good as new!"
          end
      end
