88a0ecb14e7bde58eeca24ef9142681f54a416b3
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-10 22:45:04 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -20,6 +20,9 @@ class Book
     def genre=(genre)
        @genre = genre 
      end 
+      def genre 
+        @genre
+      end
      def turn_page
        puts "Flipping the page...wow, you read fast!"
      end
